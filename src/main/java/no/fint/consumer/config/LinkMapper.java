8a237ff280774c68aa2285ec36e30e47a7d1284a
Asgeir Nilsen <asgeir@fintlabs.no>
2020-01-08 10:12:52 +0100

    Updated LinkMapper.

--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -1,28 +1,36 @@
 package no.fint.consumer.config;
 
-import no.fint.consumer.utils.RestEndpoints;
-import java.util.Map;
 import com.google.common.collect.ImmutableMap;
-
-import no.fint.model.utdanning.elev.Medlemskap;
+import no.fint.consumer.utils.RestEndpoints;
+import no.fint.model.utdanning.elev.Elevforhold;
+import no.fint.model.utdanning.elev.Undervisningsforhold;
 import no.fint.model.utdanning.kodeverk.Karakterskala;
 import no.fint.model.utdanning.timeplan.Fag;
+import no.fint.model.utdanning.timeplan.Undervisningsgruppe;
+import no.fint.model.utdanning.utdanningsprogram.Skole;
+import no.fint.model.utdanning.vurdering.Eksamensgruppe;
+import no.fint.model.utdanning.vurdering.Fravar;
+import no.fint.model.utdanning.vurdering.Karakterverdi;
+import no.fint.model.utdanning.vurdering.Vurdering;
 
-import no.fint.model.utdanning.vurdering.*;
-import no.fint.model.felles.*;
+import java.util.Map;
 
 public class LinkMapper {
 
-	public static Map<String, String> linkMapper(String contextPath) {
-		return ImmutableMap.<String,String>builder()
-			.put(Eksamensgruppe.class.getName(), contextPath + RestEndpoints.EKSAMENSGRUPPE)
-			.put(Fravar.class.getName(), contextPath + RestEndpoints.FRAVAR)
-			.put(Karakterverdi.class.getName(), contextPath + RestEndpoints.KARAKTERVERDI)
-			.put(Vurdering.class.getName(), contextPath + RestEndpoints.VURDERING)
-            .put(Karakterskala.class.getName(), "/utdanning/kodeverk/karakterskala")
-            .put(Medlemskap.class.getName(), "/utdanning/elev/medlemskap")
-            .put(Fag.class.getName(), "/utdanning/timeplan/fag")
-			.build();
-	}
+    public static Map<String, String> linkMapper(String contextPath) {
+        return ImmutableMap.<String, String>builder()
+                .put(Eksamensgruppe.class.getName(), contextPath + RestEndpoints.EKSAMENSGRUPPE)
+                .put(Fravar.class.getName(), contextPath + RestEndpoints.FRAVAR)
+                .put(Karakterverdi.class.getName(), contextPath + RestEndpoints.KARAKTERVERDI)
+                .put(Vurdering.class.getName(), contextPath + RestEndpoints.VURDERING)
+                .put(Karakterskala.class.getName(), "/utdanning/kodeverk/karakterskala")
+                .put(Elevforhold.class.getName(), "/utdanning/elev/elevforhold")
+                .put(Undervisningsforhold.class.getName(), "/utdanning/elev/undervisningsforhold")
+                .put(Fag.class.getName(), "/utdanning/timeplan/fag")
+                .put(Undervisningsgruppe.class.getName(), "/utdanning/timeplan/undervisningsgruppe")
+                .put(Skole.class.getName(), "/utdanning/utdanningsprogram/skole")
+                .build();
+    }
 
 }
+
