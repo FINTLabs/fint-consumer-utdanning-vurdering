b9da5285be1de5c1d0d62417f142736103a2c9b4
Asgeir Storesund Nilsen <asgeir@fintlabs.no>
2020-05-06 06:43:36 +0200

    Version 3.5.0 (#5)
    * Version 3.5.0.
    
    * Added missing classes.

--- src/main/java/no/fint/consumer/config/Constants.java
+++ src/main/java/no/fint/consumer/config/Constants.java
@@ -12,13 +12,16 @@ public enum Constants {
     public static final String CACHE_INITIALDELAY_EKSAMENSGRUPPE = "${fint.consumer.cache.initialDelay.eksamensgruppe:900000}";
     public static final String CACHE_FIXEDRATE_EKSAMENSGRUPPE = "${fint.consumer.cache.fixedRate.eksamensgruppe:900000}";
     
-    public static final String CACHE_INITIALDELAY_FRAVAR = "${fint.consumer.cache.initialDelay.fravar:960000}";
+    public static final String CACHE_INITIALDELAY_EKSAMENSGRUPPEMEDLEMSKAP = "${fint.consumer.cache.initialDelay.eksamensgruppemedlemskap:960000}";
+    public static final String CACHE_FIXEDRATE_EKSAMENSGRUPPEMEDLEMSKAP = "${fint.consumer.cache.fixedRate.eksamensgruppemedlemskap:900000}";
+    
+    public static final String CACHE_INITIALDELAY_FRAVAR = "${fint.consumer.cache.initialDelay.fravar:1020000}";
     public static final String CACHE_FIXEDRATE_FRAVAR = "${fint.consumer.cache.fixedRate.fravar:900000}";
     
-    public static final String CACHE_INITIALDELAY_KARAKTERVERDI = "${fint.consumer.cache.initialDelay.karakterverdi:1020000}";
+    public static final String CACHE_INITIALDELAY_KARAKTERVERDI = "${fint.consumer.cache.initialDelay.karakterverdi:1080000}";
     public static final String CACHE_FIXEDRATE_KARAKTERVERDI = "${fint.consumer.cache.fixedRate.karakterverdi:900000}";
     
-    public static final String CACHE_INITIALDELAY_VURDERING = "${fint.consumer.cache.initialDelay.vurdering:1080000}";
+    public static final String CACHE_INITIALDELAY_VURDERING = "${fint.consumer.cache.initialDelay.vurdering:1140000}";
     public static final String CACHE_FIXEDRATE_VURDERING = "${fint.consumer.cache.fixedRate.vurdering:900000}";
     
 
--- src/main/java/no/fint/consumer/utils/RestEndpoints.java
+++ src/main/java/no/fint/consumer/utils/RestEndpoints.java
@@ -5,6 +5,7 @@ public enum RestEndpoints {
 
     public static final String ADMIN = "/admin";
 	public static final String EKSAMENSGRUPPE = "/eksamensgruppe";
+	public static final String EKSAMENSGRUPPEMEDLEMSKAP = "/eksamensgruppemedlemskap";
 	public static final String FRAVAR = "/fravar";
 	public static final String KARAKTERVERDI = "/karakterverdi";
 	public static final String VURDERING = "/vurdering";
