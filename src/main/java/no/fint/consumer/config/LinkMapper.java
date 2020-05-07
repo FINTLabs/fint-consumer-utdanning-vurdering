b00b73e695e06aaded821016f1be99b5b6ba80c4
Asgeir Nilsen <asgeir@fintlabs.no>
2020-05-07 14:25:55 +0200

    Updated LinkMapper

--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -5,13 +5,12 @@ import no.fint.consumer.utils.RestEndpoints;
 import no.fint.model.utdanning.elev.Elevforhold;
 import no.fint.model.utdanning.elev.Undervisningsforhold;
 import no.fint.model.utdanning.kodeverk.Karakterskala;
+import no.fint.model.utdanning.kodeverk.Skolear;
+import no.fint.model.utdanning.kodeverk.Termin;
 import no.fint.model.utdanning.timeplan.Fag;
 import no.fint.model.utdanning.timeplan.Undervisningsgruppe;
 import no.fint.model.utdanning.utdanningsprogram.Skole;
-import no.fint.model.utdanning.vurdering.Eksamensgruppe;
-import no.fint.model.utdanning.vurdering.Fravar;
-import no.fint.model.utdanning.vurdering.Karakterverdi;
-import no.fint.model.utdanning.vurdering.Vurdering;
+import no.fint.model.utdanning.vurdering.*;
 
 import java.util.Map;
 
@@ -20,10 +19,13 @@ public class LinkMapper {
     public static Map<String, String> linkMapper(String contextPath) {
         return ImmutableMap.<String, String>builder()
                 .put(Eksamensgruppe.class.getName(), contextPath + RestEndpoints.EKSAMENSGRUPPE)
+                .put(Eksamensgruppemedlemskap.class.getName(), contextPath + RestEndpoints.EKSAMENSGRUPPEMEDLEMSKAP)
                 .put(Fravar.class.getName(), contextPath + RestEndpoints.FRAVAR)
                 .put(Karakterverdi.class.getName(), contextPath + RestEndpoints.KARAKTERVERDI)
                 .put(Vurdering.class.getName(), contextPath + RestEndpoints.VURDERING)
                 .put(Karakterskala.class.getName(), "/utdanning/kodeverk/karakterskala")
+                .put(Skolear.class.getName(), "/utdanning/kodeverk/skolear")
+                .put(Termin.class.getName(), "/utdanning/kodeverk/termin")
                 .put(Elevforhold.class.getName(), "/utdanning/elev/elevforhold")
                 .put(Undervisningsforhold.class.getName(), "/utdanning/elev/undervisningsforhold")
                 .put(Fag.class.getName(), "/utdanning/timeplan/fag")
