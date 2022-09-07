bcbdb0038ed6358fdf3152db3bd2ff2755a9c9fd
Ole Anders Eidjord <26850+oleanders@users.noreply.github.com>
2022-09-07 14:01:50 +0200

    Version 3.11.0. (#15)

--- src/main/java/no/fint/consumer/utils/RestEndpoints.java
+++ src/main/java/no/fint/consumer/utils/RestEndpoints.java
@@ -7,11 +7,14 @@ public enum RestEndpoints {
 	public static final String ANMERKNINGER = "/anmerkninger";
 	public static final String EKSAMENSGRUPPE = "/eksamensgruppe";
 	public static final String EKSAMENSGRUPPEMEDLEMSKAP = "/eksamensgruppemedlemskap";
+	public static final String ELEVFRAVAR = "/elevfravar";
 	public static final String FRAVAR = "/fravar";
 	public static final String FRAVARSOVERSIKT = "/fravarsoversikt";
 	public static final String HALVARSFAGVURDERING = "/halvarsfagvurdering";
 	public static final String HALVARSORDENSVURDERING = "/halvarsordensvurdering";
+	public static final String KARAKTERHISTORIE = "/karakterhistorie";
 	public static final String KARAKTERVERDI = "/karakterverdi";
+	public static final String SENSOR = "/sensor";
 	public static final String SLUTTFAGVURDERING = "/sluttfagvurdering";
 	public static final String SLUTTORDENSVURDERING = "/sluttordensvurdering";
 	public static final String UNDERVEISFAGVURDERING = "/underveisfagvurdering";
