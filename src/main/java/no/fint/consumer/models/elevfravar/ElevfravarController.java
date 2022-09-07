c3d0935597e83984f84d1d602a2e48e327ec6cc3
Ole Anders Eidjord <ole.anders.eidjord@vigoiks.no>
2022-09-07 13:31:01 +0200

    Version 3.11.0.

---
+++ src/main/java/no/fint/consumer/models/elevfravar/ElevfravarController.java
@@ -0,0 +1,236 @@
+package no.fint.consumer.models.elevfravar;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.google.common.collect.ImmutableMap;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import no.fint.audit.FintAuditService;
+
+import no.fint.cache.exceptions.*;
+import no.fint.consumer.config.Constants;
+import no.fint.consumer.config.ConsumerProps;
+import no.fint.consumer.event.ConsumerEventUtil;
+import no.fint.consumer.event.SynchronousEvents;
+import no.fint.consumer.exceptions.*;
+import no.fint.consumer.status.StatusCache;
+import no.fint.consumer.utils.EventResponses;
+import no.fint.consumer.utils.RestEndpoints;
+
+import no.fint.event.model.*;
+
+import no.fint.relations.FintRelationsMediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.UnknownHostException;
+import java.net.URI;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+import no.fint.model.resource.utdanning.vurdering.ElevfravarResource;
+import no.fint.model.resource.utdanning.vurdering.ElevfravarResources;
+import no.fint.model.utdanning.vurdering.VurderingActions;
+
+@Slf4j
+@Api(tags = {"Elevfravar"})
+@CrossOrigin
+@RestController
+@RequestMapping(name = "Elevfravar", value = RestEndpoints.ELEVFRAVAR, produces = {FintRelationsMediaType.APPLICATION_HAL_JSON_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
+public class ElevfravarController {
+
+    @Autowired(required = false)
+    private ElevfravarCacheService cacheService;
+
+    @Autowired
+    private FintAuditService fintAuditService;
+
+    @Autowired
+    private ElevfravarLinker linker;
+
+    @Autowired
+    private ConsumerProps props;
+
+    @Autowired
+    private StatusCache statusCache;
+
+    @Autowired
+    private ConsumerEventUtil consumerEventUtil;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private SynchronousEvents synchronousEvents;
+
+    @GetMapping("/last-updated")
+    public Map<String, String> getLastUpdated(@RequestHeader(name = HeaderConstants.ORG_ID, required = false) String orgId) {
+        if (cacheService == null) {
+            throw new CacheDisabledException("Elevfravar cache is disabled.");
+        }
+        if (props.isOverrideOrgId() || orgId == null) {
+            orgId = props.getDefaultOrgId();
+        }
+        String lastUpdated = Long.toString(cacheService.getLastUpdated(orgId));
+        return ImmutableMap.of("lastUpdated", lastUpdated);
+    }
+
+    @GetMapping("/cache/size")
+    public ImmutableMap<String, Integer> getCacheSize(@RequestHeader(name = HeaderConstants.ORG_ID, required = false) String orgId) {
+        if (cacheService == null) {
+            throw new CacheDisabledException("Elevfravar cache is disabled.");
+        }
+        if (props.isOverrideOrgId() || orgId == null) {
+            orgId = props.getDefaultOrgId();
+        }
+        return ImmutableMap.of("size", cacheService.getCacheSize(orgId));
+    }
+
+    @GetMapping
+    public ElevfravarResources getElevfravar(
+            @RequestHeader(name = HeaderConstants.ORG_ID, required = false) String orgId,
+            @RequestHeader(name = HeaderConstants.CLIENT, required = false) String client,
+            @RequestParam(defaultValue = "0") long sinceTimeStamp,
+            @RequestParam(defaultValue = "0") int size,
+            @RequestParam(defaultValue = "0") int offset,
+            HttpServletRequest request) {
+        if (cacheService == null) {
+            throw new CacheDisabledException("Elevfravar cache is disabled.");
+        }
+        if (props.isOverrideOrgId() || orgId == null) {
+            orgId = props.getDefaultOrgId();
+        }
+        if (client == null) {
+            client = props.getDefaultClient();
+        }
+        log.debug("OrgId: {}, Client: {}", orgId, client);
+
+        Event event = new Event(orgId, Constants.COMPONENT, VurderingActions.GET_ALL_ELEVFRAVAR, client);
+        event.setOperation(Operation.READ);
+        if (StringUtils.isNotBlank(request.getQueryString())) {
+            event.setQuery("?" + request.getQueryString());
+        }
+        fintAuditService.audit(event);
+        fintAuditService.audit(event, Status.CACHE);
+
+        Stream<ElevfravarResource> resources;
+        if (size > 0 && offset >= 0 && sinceTimeStamp > 0) {
+            resources = cacheService.streamSliceSince(orgId, sinceTimeStamp, offset, size);
+        } else if (size > 0 && offset >= 0) {
+            resources = cacheService.streamSlice(orgId, offset, size);
+        } else if (sinceTimeStamp > 0) {
+            resources = cacheService.streamSince(orgId, sinceTimeStamp);
+        } else {
+            resources = cacheService.streamAll(orgId);
+        }
+
+        fintAuditService.audit(event, Status.CACHE_RESPONSE, Status.SENT_TO_CLIENT);
+
+        return linker.toResources(resources, offset, size, cacheService.getCacheSize(orgId));
+    }
+
+
+    @GetMapping("/systemid/{id:.+}")
+    public ElevfravarResource getElevfravarBySystemId(
+            @PathVariable String id,
+            @RequestHeader(name = HeaderConstants.ORG_ID, required = false) String orgId,
+            @RequestHeader(name = HeaderConstants.CLIENT, required = false) String client) throws InterruptedException {
+        if (props.isOverrideOrgId() || orgId == null) {
+            orgId = props.getDefaultOrgId();
+        }
+        if (client == null) {
+            client = props.getDefaultClient();
+        }
+        log.debug("systemId: {}, OrgId: {}, Client: {}", id, orgId, client);
+
+        Event event = new Event(orgId, Constants.COMPONENT, VurderingActions.GET_ELEVFRAVAR, client);
+        event.setOperation(Operation.READ);
+        event.setQuery("systemId/" + id);
+
+        if (cacheService != null) {
+            fintAuditService.audit(event);
+            fintAuditService.audit(event, Status.CACHE);
+
+            Optional<ElevfravarResource> elevfravar = cacheService.getElevfravarBySystemId(orgId, id);
+
+            fintAuditService.audit(event, Status.CACHE_RESPONSE, Status.SENT_TO_CLIENT);
+
+            return elevfravar.map(linker::toResource).orElseThrow(() -> new EntityNotFoundException(id));
+
+        } else {
+            BlockingQueue<Event> queue = synchronousEvents.register(event);
+            consumerEventUtil.send(event);
+
+            Event response = EventResponses.handle(queue.poll(5, TimeUnit.MINUTES));
+
+            if (response.getData() == null ||
+                    response.getData().isEmpty()) throw new EntityNotFoundException(id);
+
+            ElevfravarResource elevfravar = objectMapper.convertValue(response.getData().get(0), ElevfravarResource.class);
+
+            fintAuditService.audit(response, Status.SENT_TO_CLIENT);
+
+            return linker.toResource(elevfravar);
+        }    
+    }
+
+
+
+
+    //
+    // Exception handlers
+    //
+    @ExceptionHandler(EventResponseException.class)
+    public ResponseEntity handleEventResponseException(EventResponseException e) {
+        return ResponseEntity.status(e.getStatus()).body(e.getResponse());
+    }
+
+    @ExceptionHandler(UpdateEntityMismatchException.class)
+    public ResponseEntity handleUpdateEntityMismatch(Exception e) {
+        return ResponseEntity.badRequest().body(ErrorResponse.of(e));
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity handleEntityNotFound(Exception e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.of(e));
+    }
+
+    @ExceptionHandler(CreateEntityMismatchException.class)
+    public ResponseEntity handleCreateEntityMismatch(Exception e) {
+        return ResponseEntity.badRequest().body(ErrorResponse.of(e));
+    }
+
+    @ExceptionHandler(EntityFoundException.class)
+    public ResponseEntity handleEntityFound(Exception e) {
+        return ResponseEntity.status(HttpStatus.FOUND).body(ErrorResponse.of(e));
+    }
+
+    @ExceptionHandler(CacheDisabledException.class)
+    public ResponseEntity handleBadRequest(Exception e) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(ErrorResponse.of(e));
+    }
+
+    @ExceptionHandler(UnknownHostException.class)
+    public ResponseEntity handleUnkownHost(Exception e) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(ErrorResponse.of(e));
+    }
+
+    @ExceptionHandler(CacheNotFoundException.class)
+    public ResponseEntity handleCacheNotFound(Exception e) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(ErrorResponse.of(e));
+    }
+
+}
+
