be8aedd2a432fe4d5168f9ba37147bec54a1715d
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2022-12-06 15:24:15 +0100

    FC-65-vis-load-issues-on-fravaer (#13)
    * GET_ALL events for fravar and fravarsoversikt now supports cron and events for assets is spread out
    
    * Added cron to elevfravar
    
    Co-authored-by: Trond Magnus Sevre <76692689+trondsevre@users.noreply.github.com>
    Co-authored-by: Hknots <Hknots99@gmail.com>

--- src/main/java/no/fint/consumer/config/ConsumerProps.java
+++ src/main/java/no/fint/consumer/config/ConsumerProps.java
@@ -25,6 +25,15 @@ public class ConsumerProps {
     @Value("${fint.consumer.status.created:false}")
     private boolean useCreated;
 
+    @Value("${fint.consumer.cache.event.wait.elevfravar:600000}")
+    private long eventWaitElevfravar;
+
+    @Value("${fint.consumer.cache.event.wait.fravar:600000}")
+    private long eventWaitFravar;
+
+    @Value("${fint.consumer.cache.event.wait.fravarsoversikt:600000}")
+    private long eventWaitFravarsOversikt;
+
     private Set<String> assets;
 
     @Autowired
@@ -35,6 +44,5 @@ public class ConsumerProps {
     public String[] getOrgs() {
         return assets.toArray(new String[0]);
     }
-
 }
 
