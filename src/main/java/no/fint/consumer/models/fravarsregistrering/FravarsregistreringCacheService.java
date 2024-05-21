998620d22fc9c09f28e99fbb4e5befb05f498d56
Hknots <hknots99@gmail.com>
2024-05-21 13:25:26 +0200

    more more

--- src/main/java/no/fint/consumer/models/fravarsregistrering/FravarsregistreringCacheService.java
+++ src/main/java/no/fint/consumer/models/fravarsregistrering/FravarsregistreringCacheService.java
@@ -22,8 +22,10 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 import no.fint.model.utdanning.vurdering.Fravarsregistrering;
@@ -69,19 +71,32 @@ public class FravarsregistreringCacheService extends CacheService<Fravarsregistr
         props.getAssets().forEach(this::createCache);
     }
 
-    @Scheduled(initialDelayString = Constants.CACHE_INITIALDELAY_FRAVARSREGISTRERING, fixedRateString = Constants.CACHE_FIXEDRATE_FRAVARSREGISTRERING)
+    @Scheduled(cron = Constants.CACHE_CRON_FRAVARSREGISTRERING)
     public void populateCacheAll() {
-        props.getAssets().forEach(this::populateCache);
+        Executors.newSingleThreadExecutor()
+                .execute(() -> props.getAssets().forEach(asset -> {
+                    populateCache(asset);
+                    try {
+                        Duration duration = Duration.ofMillis(props.getEventWaitFravarsregistrering());
+                        log.info("Waiting for {} minutes and {} seconds before populating cache for next asset. If this is the last asset nothing more will happen until a new schedule 🏁",
+                                duration.toMinutes(),
+                                duration.minusMinutes(duration.toMinutes()).getSeconds()
+                        );
+                        Thread.sleep(props.getEventWaitFravarsregistrering());
+                    } catch (InterruptedException e) {
+                        log.warn(e.getMessage());
+                    }
+                }));
     }
 
     public void rebuildCache(String orgId) {
-		flush(orgId);
-		populateCache(orgId);
-	}
+        flush(orgId);
+        populateCache(orgId);
+    }
 
     @Override
     public void populateCache(String orgId) {
-		log.info("Populating Fravarsregistrering cache for {}", orgId);
+        log.info("Populating Fravarsregistrering cache for {}", orgId);
         Event event = new Event(orgId, Constants.COMPONENT, VurderingActions.GET_ALL_FRAVARSREGISTRERING, Constants.CACHE_SERVICE);
         consumerEventUtil.send(event);
     }
@@ -89,16 +104,16 @@ public class FravarsregistreringCacheService extends CacheService<Fravarsregistr
 
     public Optional<FravarsregistreringResource> getFravarsregistreringBySystemId(String orgId, String systemId) {
         return getOne(orgId, systemId.hashCode(),
-            (resource) -> Optional
-                .ofNullable(resource)
-                .map(FravarsregistreringResource::getSystemId)
-                .map(Identifikator::getIdentifikatorverdi)
-                .map(systemId::equals)
-                .orElse(false));
+                (resource) -> Optional
+                        .ofNullable(resource)
+                        .map(FravarsregistreringResource::getSystemId)
+                        .map(Identifikator::getIdentifikatorverdi)
+                        .map(systemId::equals)
+                        .orElse(false));
     }
 
 
-	@Override
+    @Override
     public void onAction(Event event) {
         List<FravarsregistreringResource> data;
         if (checkFintResourceCompatibility && fintResourceCompatibility.isFintResourceData(event.getData())) {
@@ -114,9 +129,9 @@ public class FravarsregistreringCacheService extends CacheService<Fravarsregistr
         if (VurderingActions.valueOf(event.getAction()) == VurderingActions.UPDATE_FRAVARSREGISTRERING) {
             if (event.getResponseStatus() == ResponseStatus.ACCEPTED || event.getResponseStatus() == ResponseStatus.CONFLICT) {
                 List<CacheObject<FravarsregistreringResource>> cacheObjects = data
-                    .stream()
-                    .map(i -> new CacheObject<>(i, linker.hashCodes(i)))
-                    .collect(Collectors.toList());
+                        .stream()
+                        .map(i -> new CacheObject<>(i, linker.hashCodes(i)))
+                        .collect(Collectors.toList());
                 addCache(event.getOrgId(), cacheObjects);
                 log.info("Added {} cache objects to cache for {}", cacheObjects.size(), event.getOrgId());
             } else {
